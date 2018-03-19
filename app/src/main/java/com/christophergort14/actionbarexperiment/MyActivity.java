08422bf7e36a08a69bf778c6372d0a63582f5997
Chris Gort <christophergort14@augustana.edu>
2018-03-19 15:52:18 -0500

    Attempted Switching Tabs view Switch and Cases: App now crashes on start

--- app/src/main/java/com/christophergort14/actionbarexperiment/MyActivity.java
+++ app/src/main/java/com/christophergort14/actionbarexperiment/MyActivity.java
@@ -62,7 +62,6 @@ public class MyActivity extends AppCompatActivity {
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-
     }
 
 
@@ -89,41 +88,6 @@ public class MyActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_breakfast, container, false);
-            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -137,7 +101,22 @@ public class MyActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            //return PlaceholderFragment.newInstance(position + 1);
+            switch (position){
+                case 1:
+                    BreakfastFragment bTab = new BreakfastFragment();
+                    return breakfastFragment;
+                case 2:
+                    LunchFragment lTab = new LunchFragment();
+                    return lunchFragment;
+                case 3:
+                    SnackFragment sTab = new SnackFragment();
+                    return snackFragment;
+                case 4:
+                    DinnerFragment dTab = new DinnerFragment();
+                    return dinnerFragment;
+            }
+            return breakfastFragment;
         }
 
         @Override
